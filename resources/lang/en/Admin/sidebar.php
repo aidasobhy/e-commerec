2a52153372199eaf3c9eaa471c3b78b7dae803c5
Aida <aidasobhy80@gmail.com>
2023-07-31 22:36:14 +0300

    =begin branch

---
+++ resources/lang/en/Admin/sidebar.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'settings'=>'Settings',
+    'shipping method'=>'Shipping Method',
+    'free shipping'=>'Free Shipping',
+    'local shipping'=>'Local Shipping',
+    'flat rate'=>'Flat Rate',
+
+
+];
