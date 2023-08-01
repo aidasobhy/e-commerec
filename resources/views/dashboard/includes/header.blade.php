a8d11c9dba7648cf36089fcc7ddc09c96feacbb8
Aida <aidasobhy80@gmail.com>
2023-08-01 12:10:33 +0300

    =settings and authentications

--- resources/views/dashboard/includes/header.blade.php
+++ resources/views/dashboard/includes/header.blade.php
@@ -30,18 +30,18 @@
                 <ul class="nav navbar-nav float-right">
                     <li class="dropdown dropdown-user nav-item">
                         <a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                <span class="mr-1">مرجبا
+                <span class="mr-1">{{__('Admin\header.welcome')}}
                   <span
-                      class="user-name text-bold-700">  Ahmed Emam</span>
+                      class="user-name text-bold-700">{{ auth('admin')->user()->name }}</span>
                 </span>
                             <span class="avatar avatar-online">
                   <img  style="height: 35px;" src="" alt="avatar"><i></i></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href=""><i
-                                    class="ft-user"></i> تعديل الملف الشحصي </a>
+                                    class="ft-user"></i> {{__('Admin\header.edit profile')}}</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href=""><i class="ft-power"></i> تسجيل
-                                الخروج </a>
+                            <a class="dropdown-item" href="{{route('admin.logout')}}"><i class="ft-power"></i>
+                                {{__('Admin\header.log out')}} </a>
                         </div>
                     </li>
 
